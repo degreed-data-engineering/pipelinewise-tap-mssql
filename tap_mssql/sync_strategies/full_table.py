7c087d1d7dbf3960d8b22ef50f90326e3385529d
Bryan Rose <brose@degreed.com>
2022-03-20 23:53:21 -0500

    updated json stinrg

--- tap_mssql/sync_strategies/full_table.py
+++ tap_mssql/sync_strategies/full_table.py
@@ -2,6 +2,7 @@
 # pylint: disable=duplicate-code,too-many-locals,simplifiable-if-expression
 
 import copy
+import json
 import pandas as pd
 import singer
 from singer import metadata
@@ -120,12 +121,13 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version
             csv_saved += 1
             common.create_gzip(query_df, catalog_entry, csv_saved, table_stream)
             #query_df.apply(write_dataframe_record, args=(catalog_entry,stream_version, columns, table_stream, time_extracted), axis=1)
-
+        
         singer_message = {'type': 'FASTSYNC','stream':table_stream, 'version': stream_version }
         LOGGER.info('**PR** LINE 125 MESSAGE')
         LOGGER.info(singer_message)
         #singer.write_message(singer_message)
-        sys.stdout.write(str(singer_message) + '\n')
+        json_object = json.dumps(singer_message) 
+        sys.stdout.write(str(json_object) + '\n')
         sys.stdout.flush()
     else: 
 
