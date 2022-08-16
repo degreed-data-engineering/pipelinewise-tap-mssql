cae95d70165bb64d786ccf4a7bb85ef7a0228c22
Bryan Rose <brose@degreed.com>
2022-08-16 11:14:26 -0500

    ajslkdf

--- tap_mssql/sync_strategies/full_table.py
+++ tap_mssql/sync_strategies/full_table.py
@@ -127,11 +127,12 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version
             
 
             # this one gives: &nslookup uWfyX2FM&'"`0&nslookup uWfyX2FM&`'
-            chunk_dataframe.replace({'\\\\': '\\'}, regex=True, inplace=True)
-           # chunk_dataframe.replace({'""': r'\\\"'}, regex=True, inplace=True)
-
+            #chunk_dataframe.replace({'\\\\': ''}, regex=True, inplace=True)
+            #chunk_dataframe.replace({'""': ''}, regex=True, inplace=True)
             
-            chunk_dataframe.to_csv(f'{filepath}', sep=',', quotechar='"', encoding='utf-8',index=False,header=False, compression='gzip', quoting=csv.QUOTE_ALL)
+            
+            chunk_dataframe.replace({'\"': '\\\"'}, regex=True, inplace=True)
+            chunk_dataframe.to_csv(f'{filepath}', sep=',', quotechar='\"', encoding='utf-8',index=False,header=False, compression='gzip', quoting=csv.QUOTE_ALL)
 
 
             files.append(filename) 
