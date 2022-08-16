fc17b797b9925ae762a87b3bd29c7e9f7bec8647
Bryan Rose <brose@degreed.com>
2022-08-16 02:11:58 -0500

    new method

--- tap_mssql/sync_strategies/full_table.py
+++ tap_mssql/sync_strategies/full_table.py
@@ -116,7 +116,9 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version
             # chunk_dataframe.replace({'\\\\': '\\\\\\"'}, regex=True, inplace=True)
             # chunk_dataframe.replace({'\"\"': '\"'}, regex=True, inplace=True)
             
-            chunk_dataframe.replace({r'\"': '\\\\\\'}, regex=True, inplace=True)
+            #chunk_dataframe.replace({r'\"': '\\\\\\'}, regex=True, inplace=True)
+            
+            chunk_dataframe.replace({'\\"': '\\\\\\'}, regex=True, inplace=True)
             
             chunk_dataframe.to_csv(f'{filepath}', sep=',', quotechar='"', encoding='utf-8',index=False,header=False, compression='gzip', quoting=csv.QUOTE_ALL)
 
