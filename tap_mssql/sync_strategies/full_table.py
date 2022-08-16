0b55d9e371f52f3941097d993ebe58e1dfcf0f67
Bryan Rose <brose@degreed.com>
2022-08-16 10:38:05 -0500

    fixed special character

--- tap_mssql/sync_strategies/full_table.py
+++ tap_mssql/sync_strategies/full_table.py
@@ -128,7 +128,7 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns, stream_version
 
             # this one gives: &nslookup uWfyX2FM&'"`0&nslookup uWfyX2FM&`'
             chunk_dataframe.replace({'\\\\': ''}, regex=True, inplace=True)
-            chunk_dataframe.replace({'""': '\\'}, regex=True, inplace=True)
+            chunk_dataframe.replace({'"': '\\"'}, regex=True, inplace=True)
             
             chunk_dataframe.to_csv(f'{filepath}', sep=',', quotechar='"', encoding='utf-8',index=False,header=False, compression='gzip', quoting=csv.QUOTE_ALL)
 
