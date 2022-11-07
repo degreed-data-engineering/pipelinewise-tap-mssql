bdff362fda489c5a7bc06362d5a9d3ac9313eae1
Bryan Rose <brose@degreed.com>
2022-11-07 15:15:43 -0600

    reverted pyodbc version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "pendulum==1.2.0",
         "singer-python==5.9.0",
         "sqlalchemy<2.0.0",
-        "pyodbc==4.0.34",
+        "pyodbc==4.0.26",
         "backoff==1.8.0",
         "MarkupSafe==2.0.1",
         "jinja2==2.11.3",
